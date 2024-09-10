8f02d77d1daa46d63f0605d9ce69efdca7dde235
Comsea <contact@comsea.fr>
2024-09-10 08:58:36 +0200

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "UTA SAMG",
+  description: "Implantée dans les Ardennes depuis plus de trois décennies, notre entreprise spécialisée dans la sous-traitance en mécanique générale et l'usinage en commande numérique se distingue par son expertise.",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
